3d8456e2cba4c3f41d23ec5e8163b4c762f568f8
KungaK <kingkung@Kungas-MacBook-Pro.local>
2018-05-19 15:47:35 +1000

    hashquery completed and updated hashload

--- HashLoad.java
+++ HashLoad.java
@@ -2,34 +2,45 @@ import java.io.*;
 
 public class HashLoad {
    
-   static final int HASH_SIZE = 53;
+   static final int HASH_SIZE = 3700001; // HashSize&mod : number used as closest prime to 3,700,000
    static final int RECORD_SIZE = 297;
    static final int BNAME_SIZE = 200;
-   static final int OVERHEAD = 18;
+   static final int OVERHEAD = 18; // to handle the business name in front of each record
    
-   static int currentPositionInHeap = 0;
+   static int currentPositionInHeap = 0; // where the current pointer in the heap file is
    static int collisionCounter = 0;
    static int insertionCounter = 0;
-   static boolean lastRecordFlag = false;
-   static int trailingWhiteSpace;
+   static boolean lastRecordFlag = false; // To flag when its the last record in the 
+   static int trailingWhiteSpace; // used to store the trailing white space calculated
    
+   // Initialize hash file by printing -1s for all the viable positions in the hash
+   // to represent null. Will be checked later when inserting values in that position.
+   // cannot be any positive numbers or 0 as on comparison there could be duplicates.
    public void initialiseHashFile(int pageSize) {
-      File hashFile = new File("testhash." + pageSize);
+      File hashFile = new File("hash." + pageSize);
       try {
          RandomAccessFile raf = new RandomAccessFile(hashFile,"rw");
          
          for(int i=0; i<HASH_SIZE; i++) {
-            raf.writeInt(0);
+            raf.writeInt(-1);
          }
+         
+         raf.close();
       } catch(IOException e) {
          System.out.println(hashFile + " not found.");
       }
+      
    }
    
+   // Function to read the each page into each record and extract the name portion of
+   // the record. Hash that name obtained which will be used as the index to store 
+   // the current position of the record in the hash file. 
    public void hashFunction(int pageSize) {
       
-      File heapFile = new File("test." + pageSize);
-      File hashFile = new File("testhash." + pageSize);
+      File heapFile = new File("heap." + pageSize);
+      File hashFile = new File("hash." + pageSize);
+      
+      // Used to store the empty space at the end of each page.
       trailingWhiteSpace = pageSize - (13 * RECORD_SIZE);
       
       boolean isNextPage = true;
@@ -44,9 +55,12 @@ public class HashLoad {
             byte[] page = new byte[pageSize];
             fis.read(page, 0, pageSize);
             
+            // Check if the end of the file has been reached.
+            // Done so by coverting byte to string of the page.
+            // Trim that string and if the length is 0 then we 
+            // know we are at the end.
             String pageCheck = new String(page);
             if(pageCheck.trim().length() == 0) {
-               System.out.println("Should break page");
                isNextPage = false;
                break;
             }
@@ -54,51 +68,71 @@ public class HashLoad {
             isNextRecord = true;
             while(isNextRecord) {
                for(int i=0; i<page.length; i+=RECORD_SIZE) {
-                  if(i == 3564) { //lastrecord
+                  
+                  // when i = 3564 then we know its the amout of max
+                  // record that can be stored in that page and it is the last
+                  // record.
+                  if(i == 3564) {
                      lastRecordFlag = true;
                   }
+                  
+                  // when i = 3861 then we know no more records can be in this page
+                  // thus it is the end of the page.
                   if(i == 3861) {
-                     System.out.println("Last record reached in current page");
                      isNextRecord = false;
                      break;
                   }
-                  
                   byte[] bName = new byte[BNAME_SIZE];
                   System.arraycopy(page, i+OVERHEAD, bName, 0, 200);
-
                   String name = new String(bName).trim();
                   
-                  System.out.println(counter + " " + name);
-                  counter++;
+                  // used to see how many records read
+                  System.out.println(counter);
+                  counter++; 
                   
                   int hash_Name = Math.abs(name.hashCode());
-                  
                   int hashIndex = hash_Name % HASH_SIZE;
-                  
                   hashWrite(hashIndex, pageSize, hashRaf);
-                  
                }
             }
          }
+         
+         fis.close();
+         hashRaf.close();
+         
+         System.out.println("Completed");
+         System.out.println("Insertions: " + insertionCounter);
+         System.out.println("Collisions: " + collisionCounter);
       } catch (FileNotFoundException e) {
-         // TODO Auto-generated catch block
          e.printStackTrace();
       } catch (IOException e) {
-         // TODO Auto-generated catch block
          e.printStackTrace();
       }
    }
 
+   // Function to write the current position in the heap file to the hash file with the
+   // calculated hashname which will be the index in the hashfile. 
    public void hashWrite(int hashIndex, int pageSize, RandomAccessFile hashRaf) {
       
       while(true) {
          try {
             hashRaf.seek(hashIndex);
-            int checkPosHash = hashRaf.read();
+            int checkPosHash = hashRaf.readInt();
             hashRaf.seek(hashIndex);
             
-            if(checkPosHash == 0) {
-               hashRaf.write(currentPositionInHeap);
+            // If the hashIndex in the hashfile is equal to 0 (which was set) then
+            // it is empty and can be written into. 
+            // Otherwise increment collision counter and the hashindex by 4 (1 int = 4 bytes) 
+            // which is to get to the next 0 in the hashfile.
+            if(checkPosHash == -1) {
+               hashRaf.writeInt(currentPositionInHeap);
+               insertionCounter++;
+               
+               // If the lastRecordFlag has been changed then it is the last record
+               // in that page thus add the trailing white space to increment to the 
+               // position of the next record in the new page. 
+               // Otherwise just increment by the record size as there are more records
+               // in the current read page.
                if(lastRecordFlag == true) {
                   currentPositionInHeap += RECORD_SIZE + trailingWhiteSpace;
                   lastRecordFlag=false;
@@ -115,11 +149,10 @@ public class HashLoad {
          }
       }
    }
-
-   public static void main(String[] args) {
-      
-      HashLoad hashLoad = new HashLoad();
    
+   // Main Method
+   public static void main(String[] args) {
+      HashLoad hashLoad = new HashLoad();
       try {
          String input = args[0];
          int pageSize = Integer.parseInt(input);
@@ -131,5 +164,4 @@ public class HashLoad {
          System.out.println("Invalid pagesize");
       }
    }
-
 }
--- HashQuery.java
+++ HashQuery.java
@@ -1,9 +1,88 @@
+import java.io.*;
+import java.util.Arrays;
 
 public class HashQuery {
 
-   public static void main(String[] args) {
-      // TODO Auto-generated method stub
+   public static final int MOD = 3700001; // used to hash strings in HashLoad.java
+
+   // Convert the input give in commandline into and array except for the last value of
+   // the input given as that will be the pagesize. From the array create a String. This will
+   // be the name of the business and hash that name the same way done int HashLoad.java
+   public void convertInput(String[] args) throws IOException {
+      String[] names = Arrays.copyOfRange(args, 0, args.length - 1);
+      String spageSize = args[args.length - 1];
+
+      String text = String.join(" ", names);
+      int pageSize = Integer.parseInt(spageSize);
+
+      File heapFile = new File("heap." + pageSize);
+      File hashFile = new File("hash." + pageSize);
+
+      int hashText = Math.abs(text.hashCode());
+      int hashIndex = hashText % MOD;
+
+      searchForInput(hashIndex, heapFile, hashFile);
+   }
+
+   // Read the hashFile with the given hashIndex to get the index of the record in the heap file.
+   // Once content at hashIndex read use that to go to the index of the record and print those 
+   // results out.
+   public void searchForInput(int hashIndex, File heapFile, File hashFile) throws IOException {
+
+      // Read hashfile using the hashIndex and get its contents.
+      RandomAccessFile hashRaf = new RandomAccessFile(hashFile, "r");
+
+      hashRaf.seek(hashIndex);
+      int positionInHeap = hashRaf.readInt();
+
+      // Use the read input from above to go to the location of the record and print those values.
+      RandomAccessFile heapRaf = new RandomAccessFile(heapFile, "r");
+      byte[] record = new byte[297];
+      heapRaf.seek(positionInHeap);
+      heapRaf.read(record, 0, positionInHeap + 297);
+
+      // Copy portions of the byte array related to its corresponding key.
+      byte[] name = Arrays.copyOfRange(record, 18, 218);
+      byte[] status = Arrays.copyOfRange(record, 218, 230);
+      byte[] dor = Arrays.copyOfRange(record, 230, 240);
+      byte[] doc = Arrays.copyOfRange(record, 240, 250);
+      byte[] rd = Arrays.copyOfRange(record, 250, 260);
+      byte[] fsn = Arrays.copyOfRange(record, 260, 270);
+      byte[] psor = Arrays.copyOfRange(record, 270, 273);
+      byte[] abn = Arrays.copyOfRange(record, 273, 293);
+
+      // Convert the byte arrays into Strings.
+      String B_name = new String(name);
+      String B_status = new String(status);
+      String B_dor = new String(dor);
+      String B_doc = new String(doc);
+      String B_rd = new String(rd);
+      String B_fsn = new String(fsn);
+      String B_psor = new String(psor);
+      String B_abn = new String(abn);
+
+      // Print all the values.
+      System.out.println("Business Name: " + B_name);
+      System.out.println("Status: " + B_status);
+      System.out.println("Date Of Registration: " + B_dor);
+      System.out.println("Date Of Cancellation: " + B_doc);
+      System.out.println("Renewal Date: " + B_rd);
+      System.out.println("Former State Number: " + B_fsn);
+      System.out.println("Previous State of Registration: " + B_psor);
+      System.out.println("ABN: " + B_abn);
+
+      hashRaf.close();
+      heapRaf.close();
 
    }
 
+   // Main Method
+   public static void main(String[] args) {
+      HashQuery hashQuery = new HashQuery();
+      try {
+         hashQuery.convertInput(args);
+      } catch (Exception e) {
+         e.printStackTrace();
+      }
+   }
 }
